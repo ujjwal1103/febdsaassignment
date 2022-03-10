45849f326f5d5c0f6dc7035212b6cee55eba3b54
Ujjwal Lade <ujjwallade11@gmail.com>
2022-03-10 20:49:59 +0530

    second

---
+++ march5/que1.js
@@ -0,0 +1,24 @@
+let matrics = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,1,2,3],
+    [4,5,6,7]
+]
+
+console.log(matrics);
+
+let frist = matrics[2]
+console.log(frist);
+
+
+let second = matrics[0];
+console.log(second);
+
+
+let thired = matrics[2][0]
+console.log(thired);
+
+
+let fourth = matrics[3][3]
+console.log(fourth);
+
